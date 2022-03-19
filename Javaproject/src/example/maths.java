cef4df014184bd138b969289d901f14e0c74b402
Abbas <abbasrizvi200209@gmail.com>
2022-03-19 13:28:22 -0700

    abbas

---
+++ Javaproject/src/example/maths.java
@@ -0,0 +1,13 @@
+package example;
+
+public class maths {
+	public static void main(String args[]) {
+		
+		double a = -1309;
+		double b = 3434.23;
+		
+		double result = Math.abs(a);
+			
+		System.out.println(result);
+	}
+}
